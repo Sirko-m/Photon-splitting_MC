7927b38cb7c809755e01d58d7eb53c2738fcbcbb
Sirko-m <85785972+Sirko-m@users.noreply.github.com>
2021-09-13 18:20:13 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ h_dash = 6.582119569*10**(-16) / 510 #Blanc constans in m_ec^2 (I think) #There 
 
 #parameters
 B_cr = 4.41 * 10**13 #critical quantum value
+B_prim = 0.7 
 B_0 = 0.7 * B_cr #aprox. of PSR 1509-58
+
+B = lambda r : B_0 * r**(-3) # dipol magnetic field magnitude in flat spacetime 
+
 theta = 0 #magnetic colitude
 theta_k = 0 #angle 
 z_0 = 0 #distance from the surface 
@@ -24,10 +28,16 @@ N_0 = 10**6 #number of photons simulated
 N = lambda epsilon : N_0 *  epsilon ** (-1. * alpha)
 
 #attenuation coefficient for photon splitting for _|_ -> || || polarization
-T_sp = lambda thetha_k, omega :  1223575244542.2058 * omega * (math.sin(thetha_k))**6 ##aprox. value I got from all the constants in ex. 1 and 3 from paper for B' = 0.7 
+T_sp = lambda theta_k, omega :  1.3631218166086345*10**(-24) * omega * (math.sin(theta_k))**6 ##aprox. value I got from all the constants in ex. 1 and 3 from paper for B' = 0.7 
 
 #attenuation coefficient for pair production
-#TBA
+p = lambda j, k, omega, theta_k : (omega**2 / 4 * math.sin(theta_k)**2 - 1 - (j + k) * B_prim + ((j-k)*B_prim / (omega*math.sin(theta_k)))**2 )**(1/2)
+E_0 = lambda omega, theta_k : (1 + p(0, 1, omega, theta_k)**2)**(1/2)
+E_1 = lambda omega, theta_k : (1 + p(0, 1, omega, theta_k)**2 + 2*B_prim)**(1/2)
+# || ||
+T_pp0 = lambda theta_k, omega :  2.6463117553040002*10**(-14) * e**(- omega**2 / 1.4 * math.sin(theta_k))/(omega**2 * math.sin(theta_k) * (omega**2/4 * math.sin(theta_k)**2 - 1)**(1/2))
+# _|_
+T_pp1 = lambda theta_k, omega :  2.6463117553040002*10**(-14) * e**(- omega**2 / 1.4 * math.sin(theta_k)) * (E_0(omega, theta_k)*E_1(omega, theta_k) + 1 + p(0, 1, omega, theta_k)**2) /(omega**2 * math.sin(theta_k) * p(0, 1, omega, theta_k))
 
 
 def get_rand_pol(): #random polarization
@@ -37,16 +47,12 @@ def get_rand_pol(): #random polarization
     else:
         return 0
 
-def get_rand_energy(minimum, maximum):
-    size = maximum - minimum
-    u = random.uniform(0,1)
-    return e_min + size*u
 
-def power_law_distribution(N_0, minimum, maximum):
-    size = maximum - minimum
-    rand = random.uniform(0,1)
-    u = e_min + size*rand
-    return (u/N_0)**(1/alpha)
+def power_distribution(alpha = 1.6):
+    u = random.uniform(0,1)
+    rand = - 14.85 + 15.85*u
+    x = pow(1.-rand,-1./(alpha-1.))
+    return x
 
 
 class Photon:
@@ -54,46 +60,18 @@ class Photon:
         self.pol = get_rand_pol()
         self.energy = energy
 
-    def energy_to_omega(self, energy):
-        return energy/h_dash
+    def get_pol(self):
+        return self.pol 
+
+    def get_energy(self):
+        return self.energy/h_dash
 
 def main():
     for in range(N_0):
-        energy = get_rand_energy(e_min, e_max) 
-        for in range(N(energy)):
-            Random_Photon = Photon(1, energy)
-
-
-
-def f_of_x(x):
-    """
-    This is the main function we want to integrate over.
-    Args:
-    - x (float) : input to function; must be in radians
-    Return:
-    - output of function f(x) (float)
-    """
-    return (e**(-1*x))/(1+(x-1)**2)
-
-def crude_monte_carlo(num_samples=5000):
-    """
-    This function performs the Crude Monte Carlo for our
-    specific function f(x) on the range x=0 to x=5.
-    Notice that this bound is sufficient because f(x)
-    approaches 0 at around PI.
-    Args:
-    - num_samples (float) : number of samples
-    Return:
-    - Crude Monte Carlo estimation (float)
-    
-
-    lower_bound = 0
-    upper_bound = 5
-    
-    sum_of_samples = 0
-    for i in range(num_samples):
-        x = get_rand_number(lower_bound, upper_bound)
-        sum_of_samples += f_of_x(x)
-    
-    return (upper_bound - lower_bound) * float(sum_of_samples/num_samples)
-    """
+        energy = power_distribution() 
+        Random_Photon = Photon(1, energy)
+        R1 = random.uniform(0,1)
+        R2 = random.uniform(0,1)
+        P_s = math.exp( -scipy.integrate.quad( T_sp(theta_k, Random_Photon.get_energy()) , 0, s))
+        P_pp0 = math.exp( -scipy.integrate.quad( T_pp0(theta_k, Random_Photon.get_energy()) , 0, s))
+        P_pp1 = math.exp( -scipy.integrate.quad( T_pp1(theta_k, Random_Photon.get_energy()) , 0, s))
--- power_law_distribution.py
+++ power_law_distribution.py
@@ -5,13 +5,16 @@ import math
 def power_distribution(n = 100, alpha = 1.6):
     x=[]
     for i in range(n):
-        rand = random.uniform(0,1)
-        u = - 14.85 + 15.85*rand
-        x.append(pow(1.-u,-1./(alpha-1.)))
+        u = random.uniform(0,1)
+        rand = - 14.85 + 15.85*u
+        x.append(pow(1.-rand,-1./(alpha-1.)))
     return x
 
-x = power_distribution()
+x = power_distribution(1000000)
 print(x)
 
-y = plt.hist(x, bins=[0.01, 0.1, 1, 10,100])
+'''
+y = plt.hist(x, bins=1000, range= (0.01, 100)) #[0.01, 0.1, 1, 10,100]
+plt.xscale('log')
 plt.show()
+'''
